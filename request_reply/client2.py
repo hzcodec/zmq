c2344a0fe46e970f1026817499ac4d58976f2b87
heinz <heinz.samuelsson@gmail.com>
2017-12-12 20:43:03 +0100

    client2/server2 added

--- request_reply/client2.py
+++ request_reply/client2.py
@@ -10,8 +10,8 @@ socket.connect("tcp://localhost:5555")
 
 #  Do 10 requests, waiting each time for a response
 for request in range(10):
-    print('Sending request %s' % request)
-    socket.send(b"Hello")
+    print('Sending request from client %s' % request)
+    socket.send(b"Message from client")
 
     #  Get the reply.
     message = socket.recv()
