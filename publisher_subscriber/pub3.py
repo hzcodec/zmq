9fbf0cb1064c4acaab7e1e51b017f7d1afebb0cd
heinz.samuelsson <heinz.samuelsson@unjo.com>
2023-01-08 16:54:41 +0100

    Cleanup code

--- publisher_subscriber/pub3.py
+++ publisher_subscriber/pub3.py
@@ -1,13 +1,15 @@
 import time
 import zmq
 
-host = '127.0.0.1'
-port = '5001'
+TOPIC = b"Volt"
+
+HOST = '127.0.0.1'
+PORT = '5001'
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 
-socket.bind('tcp://{}:{}'.format(host, port))
+socket.bind('tcp://{}:{}'.format(HOST, PORT))
 time.sleep(1)
 
 value = 0.0
@@ -16,7 +18,7 @@ str_val = str(value)
 byte_val = str_val.encode()
 
 while True:
-    socket.send_multipart([b"Voltage", byte_val])
+    socket.send_multipart([TOPIC, byte_val, b"Current value"])
     time.sleep(1)
 
     value += .1
--- publisher_subscriber/sub3.py
+++ publisher_subscriber/sub3.py
@@ -1,23 +1,24 @@
 import zmq
 
-host = '127.0.0.1'
-port = '5001'
+TOPIC = b"Volt"
+HOST = '127.0.0.1'
+PORT = '5001'
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
-socket.connect('tcp://{}:{}'.format(host, port))
+socket.connect('tcp://{}:{}'.format(HOST, PORT))
 
-socket.subscribe(b'Voltage')
+socket.subscribe(TOPIC)
 
 while True:
     rv = socket.recv_multipart()
     print('[SUB] - Topic:', rv[0].decode('ascii'))
     print('[SUB] - Message:', rv[1].decode('ascii'))
+    print('[SUB] - Message:', rv[2].decode('ascii'))
 
     str_val = rv[1].decode('utf-8')
     f_val = float(str_val)
-    print(f_val*2.0)
 
 socket.close()
 context.term()
