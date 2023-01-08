f9c3d0b5558a1012732898e47a11f0730d7de930
heinz.samuelsson <heinz.samuelsson@unjo.com>
2023-01-08 17:13:19 +0100

    Add sub32 and new topic

--- publisher_subscriber/pub3.py
+++ publisher_subscriber/pub3.py
@@ -2,6 +2,7 @@ import time
 import zmq
 
 TOPIC = b"Volt"
+TOPIC2 = b"Amps"
 
 HOST = '127.0.0.1'
 PORT = '5001'
@@ -13,18 +14,26 @@ socket.bind('tcp://{}:{}'.format(HOST, PORT))
 time.sleep(1)
 
 value = 0.0
+value2 = 0.0
 
 str_val = str(value)
 byte_val = str_val.encode()
+byte_val2 = str_val.encode()
 
 while True:
     socket.send_multipart([TOPIC, byte_val, b"Current value"])
+    socket.send_multipart([TOPIC2, b"Jennie", byte_val2])
     time.sleep(1)
 
     value += .1
     value = round(value, 2)
     str_val = str(value)
+
+    value2 = value * 5
+    str_val2 = str(value2)
+
     byte_val = str_val.encode()
+    byte_val2 = str_val2.encode()
 
     print('[PUB] - value:', value)
 
---
+++ publisher_subscriber/sub32.py
@@ -0,0 +1,21 @@
+import zmq
+
+TOPIC = b"Amps"
+HOST = '127.0.0.1'
+PORT = '5001'
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+socket.connect('tcp://{}:{}'.format(HOST, PORT))
+
+socket.subscribe(TOPIC)
+
+while True:
+    rv = socket.recv_multipart()
+    print('[SUB] - Topic:', rv[0].decode('ascii'))
+    print('[SUB] - Message:', rv[1].decode('ascii'))
+    print('[SUB] - Message:', rv[2].decode('ascii'))
+
+socket.close()
+context.term()
