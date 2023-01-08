c4c0bb9fbf98cb9695070ca3e56cd16a8d9b4ef2
heinz.samuelsson <heinz.samuelsson@unjo.com>
2023-01-08 13:38:16 +0100

    Fix string to float

--- publisher_subscriber/sub3.py
+++ publisher_subscriber/sub3.py
@@ -15,5 +15,9 @@ while True:
     print('[SUB] - Topic:', rv[0].decode('ascii'))
     print('[SUB] - Message:', rv[1].decode('ascii'))
 
+    str_val = rv[1].decode('utf-8')
+    f_val = float(str_val)
+    print(f_val*2.0)
+
 socket.close()
 context.term()
